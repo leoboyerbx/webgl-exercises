c2507391500783a538b076c65543e41ea282cebe
Léo Boyer <contact@leoboyer.fr>
2022-03-23 15:45:22 +0100

    exo 2 working

--- main.ts
+++ main.ts
@@ -12,11 +12,19 @@ gl.clear(gl.COLOR_BUFFER_BIT)
 
 const loadShader = (gl, source, type) => {
     const shader = gl.createShader(type)
-    gl.shaderSource(shader,  source)
+    gl.shaderSource(shader, source)
     gl.compileShader(shader)
     return shader
 }
-
+class PointData {
+    public coords: Array<number>
+    public color: Array<number>
+    constructor(coords: Array<number>, color: Array<number>) {
+        this.coords = coords
+        this.color = color
+    }
+}
+const points: Array<PointData> = []
 const vShader = loadShader(gl, vertexShaderSource, gl.VERTEX_SHADER)
 const fShader = loadShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER)
 
@@ -27,4 +35,31 @@ gl.linkProgram(program)
 gl.validateProgram(program)
 gl.useProgram(program)
 
-gl.drawArrays(gl.POINTS, 0, 1)
+const positionAttrib = gl.getAttribLocation(program, 'aPosition')
+const colorAttrib = gl.getUniformLocation(program, 'uColor')
+const render = () => {
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    points.forEach(point => {
+        gl.vertexAttrib2fv(positionAttrib, point.coords)
+        gl.uniform4fv(colorAttrib, point.color)
+        gl.drawArrays(gl.POINTS, 0, 1)
+    })
+}
+
+
+const canvasRect = canvas.getBoundingClientRect()
+canvas.addEventListener('click', (e: PointerEvent) => {
+    const {width, height} = canvasRect
+    const x = (e.offsetX / width) * 2 - 1
+    const y = (e.offsetY / height) * 2 - 1
+
+    const color = x > 0
+        ? [1, 0, 0, 1]
+        : [0, 0, 1, 1]
+
+    points.push(new PointData([x, -y], color))
+
+    render()
+})
+
+render()
--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -1,4 +1,6 @@
+attribute vec2 aPosition;
+
 void main() {
- gl_Position = vec4(-0.25, 0.0, 0.0, 1.0);
+ gl_Position = vec4(aPosition, 0.0, 1.0);
  gl_PointSize = 10.0;
 }

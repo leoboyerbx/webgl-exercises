680a53abe4b126c9665b8e36c49b47903d70bad1
Léo Boyer <contact@leoboyer.fr>
2022-03-23 14:06:00 +0100

    exo 1

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
 import './style.css'
+// @ts-ignore
+import vertexShaderSource from './vertexShader.glsl'
+// @ts-ignore
+import fragmentShaderSource from './fragmentShader.glsl'
 
 const canvas: HTMLCanvasElement = document.querySelector('#canvas')
 const gl = canvas.getContext('webgl')
 
-gl.clearColor(1, 0.7, 0, 1)
+gl.clearColor(0, 0, 0, 1)
 gl.clear(gl.COLOR_BUFFER_BIT)
+
+const loadShader = (gl, source, type) => {
+    const shader = gl.createShader(type)
+    gl.shaderSource(shader,  source)
+    gl.compileShader(shader)
+    return shader
+}
+
+const vShader = loadShader(gl, vertexShaderSource, gl.VERTEX_SHADER)
+const fShader = loadShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER)
+
+const program = gl.createProgram()
+gl.attachShader(program, vShader)
+gl.attachShader(program, fShader)
+gl.linkProgram(program)
+gl.validateProgram(program)
+gl.useProgram(program)
+
+gl.drawArrays(gl.POINTS, 0, 1)

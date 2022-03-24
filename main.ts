b514e2bcac14fcbca2aa37a78c968782f1852c01
Léo Boyer <contact@leoboyer.fr>
2022-03-24 08:36:11 +0100

    broken

--- main.ts
+++ main.ts
@@ -1,80 +1,163 @@
 import './style.css'
-// @ts-ignore
-import vertexShaderSource from './shaders/vertexShader.glsl'
-// @ts-ignore
-import fragmentShaderSource from './shaders/fragmentShader.glsl'
+import WebGLShaderUtils from './lib/webgl-shader-utils'
+import WebGLHelpers from './lib/webgl-helpers'
+import WebGLUtils from './lib/webgl-utils'
+/*
+            * Draw points under mouse clicks
+            *
+            * When the center of the point is on the left half of the canvas, the points is red
+            * When the center of the point is on the right half of the canvas, the points is blue
+            */
 
-const canvas: HTMLCanvasElement = document.querySelector('#canvas')
-const gl = canvas.getContext('webgl')
+// Keep the WebGL context global to make code easier to read
+let gl
 
-gl.clearColor(0, 0, 0, 1)
-gl.clear(gl.COLOR_BUFFER_BIT)
+// Use only one global context object to keep code clear
+let points = []
 
-function loadShader(gl, source, type) {
-    const shader = gl.createShader(type);
+class Point
+{
+    private x: number;
+    private y: number;
+    private size: number;
+    private color: Array<number>;
+    private program: WebGLProgram;
+    private attributes: any;
+    private uniforms: any;
 
-    // Envoyer le source à l'objet shader
+    constructor(x, y, size, color)
+    {
+        /*
+         * Use the 2D clip space coordinate to create the point
+         * It will be used to set the vertex shader position attribute
+         */
+        this.x = x
+        this.y = y
 
-    gl.shaderSource(shader, source);
-
-    // Compiler le programme shader
-
-    gl.compileShader(shader);
-
-    // Vérifier s'il a ét compilé avec succès
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-        gl.deleteShader(shader);
-        return null;
-    }
-    return shader;
-}
-
-class PointData {
-    public coords: Array<number>
-    public color: Array<number>
-    constructor(coords: Array<number>, color: Array<number>) {
-        this.coords = coords
+        this.size = size
         this.color = color
+
+        this.program = null
+        this.attributes = {}
+        this.uniforms = {}
+
+        this.initProgram()
+    }
+
+    static get vertexShader()
+    {
+        return WebGLShaderUtils.shaderArrayToString([
+            "attribute vec2 aPosition;",
+            "attribute float aPointSize;",
+            "",
+            "void main()",
+            "{",
+            // gl_Position is a vec4 value in clip space (-1 .. +1)
+            "   gl_Position = vec4(aPosition, 0.0, 1.0);",
+
+            // gl_PointSize should be set with a value in pixels
+            "   gl_PointSize = aPointSize;",
+            "}"
+        ])
+    }
+
+    static get fragmentShader()
+    {
+        return WebGLShaderUtils.shaderArrayToString([
+            "precision mediump float;",
+            "",
+            "uniform vec3 uColor;",
+            "",
+            "void main()",
+            "{",
+            // The fragment color has 4 channels (r, g, b, a)
+            "   gl_FragColor = vec4(uColor, 1.0);",
+            "}"
+        ])
+    }
+
+    initProgram()
+    {
+        // Create the program with shader code
+        this.program = WebGLShaderUtils.createProgram(gl, Point.vertexShader, Point.fragmentShader)
+
+        // Use the program to allow access to attributes and uniforms location
+        gl.useProgram(this.program)
+
+        // Get all attributes locations
+        // This will allow to set the values from the javascript code with the gl.vertexAttrib function
+        this.attributes.position = WebGLHelpers.getAttributeLocation(this.program, "aPosition")
+        this.attributes.pointSize = WebGLHelpers.getAttributeLocation(this.program, "aPointSize")
+
+        // Get all uniforms locations
+        // This will allow to set the values from the javascript code with the gl.uniform function
+        this.uniforms.color = WebGLHelpers.getUniformLocation(this.program, "uColor")
+    }
+
+    draw()
+    {
+        gl.useProgram(this.program)
+
+        gl.vertexAttrib2f(this.attributes.position, this.x, this.y)
+        gl.vertexAttrib1f(this.attributes.pointSize, this.size)
+        gl.uniform3fv(this.uniforms.color, this.color)
+
+        gl.drawArrays(gl.POINTS, 0, 1)
     }
 }
-const points: Array<PointData> = []
-const vShader = loadShader(gl, vertexShaderSource, gl.VERTEX_SHADER)
-const fShader = loadShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER)
 
-const program = gl.createProgram()
-gl.attachShader(program, vShader)
-gl.attachShader(program, fShader)
-gl.linkProgram(program)
-gl.validateProgram(program)
-gl.useProgram(program)
+function init()
+{
+    const canvas: HTMLCanvasElement = document.querySelector("#canvas")
+    gl = canvas.getContext('webgl')
 
-const positionAttrib = gl.getAttribLocation(program, 'aPosition')
-const colorAttrib = gl.getUniformLocation(program, 'uColor')
-const render = () => {
+    // Set the default value of the color buffer
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    // Clear the color buffer at init to clear the canvas
     gl.clear(gl.COLOR_BUFFER_BIT)
+    return canvas
+}
+
+/*
+ * Draw all the points stored in the array
+ */
+function drawPoints()
+{
+    // Clear the color buffer before drawing all the points
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
     points.forEach(point => {
-        gl.vertexAttrib2fv(positionAttrib, point.coords)
-        gl.uniform4fv(colorAttrib, point.color)
-        gl.drawArrays(gl.POINTS, 0, 1)
+        point.draw()
     })
 }
 
+/*
+ * Click handler
+ * Convert mouse click coordinates from the canvas space into clip space
+ * Don't draw the point directly, the color buffer flush have lose previous
+ * draw calls. Store the point and draw the whole array in a row instead
+ */
+function click(event)
+{
+    const rect = event.target.getBoundingClientRect()
 
-const canvasRect = canvas.getBoundingClientRect()
-canvas.addEventListener('click', (e: PointerEvent) => {
-    const {width, height} = canvasRect
-    const xVal = e.offsetX / width
-    const x = xVal * 2 - 1
-    const yVal = (e.offsetY / height)
-    const y = -(yVal * 2 - 1)
+    let xClip = (2 * (event.clientX - rect.left) / rect.width) - 1
+    let yClip = -((2 * (event.clientY - rect.top) / rect.height) - 1)
 
-    const color = [1 - xVal, yVal, xVal, 1]
+    /*
+     * Set the uniform color value for the fragment shader depending on the
+     * x-axis value, red points on the left half, blue points on the right half
+     */
+    let color = xClip < 0 ? [1.0, 0.0, 0.0] : [0.0, 0.0, 1.0]
+    let point = new Point(xClip, yClip, 10, color)
 
-    points.push(new PointData([x, y], color))
+    // Store the new point in the points array
+    points.push(point)
 
-    render()
-})
+    // Draw the whole set of points
+    drawPoints()
+}
 
-render()
+const canvas = init()
+canvas.onmouseup = click
